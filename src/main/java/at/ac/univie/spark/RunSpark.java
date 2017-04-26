c0cda30e7ac031a09b9c79977c7b7660d43f10b5
Martin Sipiczki <sipiczki45@gmail.com>
2017-04-26 13:07:26 +0200

    tried copying from spark site we'll see

--- src/main/java/at/ac/univie/spark/RunSpark.java
+++ src/main/java/at/ac/univie/spark/RunSpark.java
@@ -68,12 +68,21 @@ class RunSpark{
 					}
 				});
 		parsedData.cache();
-		KMeans kmeans = new KMeans();
-		KMeansModel clusters = kmeans.run(parsedData.rdd());
+		// Cluster the data into two classes using KMeans
+		int numClusters = 2;
+		int numIterations = 20;
+		KMeansModel clusters = KMeans.train(parsedData.rdd(), numClusters, numIterations);
+
 		System.out.println("Cluster centers:");
-		for(Vector center:clusters.clusterCenters()){
-			System.out.println("Cluster 1: " + center);
+		for (Vector center: clusters.clusterCenters()) {
+		  System.out.println(" " + center);
 		}
+		double cost = clusters.computeCost(parsedData.rdd());
+		System.out.println("Cost: " + cost);
+
+		// Evaluate clustering by computing Within Set Sum of Squared Errors
+		double WSSSE = clusters.computeCost(parsedData.rdd());
+		System.out.println("Within Set Sum of Squared Errors = " + WSSSE);
 
 		sc.stop();
 		
